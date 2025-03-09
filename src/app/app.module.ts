3dff8e8f215753d84482feaab53f36b7a166f592
SergeyM <>
2025-03-09 15:29:04 +0300

    Add material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { material } from './shared/material.modules';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NoopAnimationsModule],
-  providers: [],
-  bootstrap: [AppComponent],
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		NoopAnimationsModule,
+		...material
+	],
+	exports: [],
+	providers: [],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
